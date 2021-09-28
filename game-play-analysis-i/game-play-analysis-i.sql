4486d8a5fa3d9dd0af38a51485c7cf320158a0df
Rohan Subhash Yewale <46860673+rhnyewale@users.noreply.github.com>
2021-09-28 14:52:33 -0400

    Time: 3807 ms (34.77%), Space: 0B (100.00%) - LeetHub

--- game-play-analysis-i/game-play-analysis-i.sql
+++ game-play-analysis-i/game-play-analysis-i.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
-SELECT DISTINCT player_id, MIN(event_date) as "first_login"
-FROM Activity
+/* Write your T-SQL query statement below */
+SELECT player_id, MIN(event_date) as first_login
+FROM activity
 GROUP BY player_id
